0ed1d14504fb435fcae8f0370e7f29b5ae6a882a
Anurag Raju <anuragrajuv@gmail.com>
2024-09-18 21:35:41 +0530

    add a controller and a router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,7 @@
+module.exports.home = function(req,res){
+    return res.end('<h1>Home controller Express is up for codeial</h1>')
+}
+
+module.exports.actionName = function(req,res){
+    return res.end("humne kya bigada tha tera")
+}
