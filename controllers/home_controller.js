25afe713c5ae10c2eb31b49dffc2e1f68c60be44
Anurag Raju <anuragrajuv@gmail.com>
2024-09-20 13:56:11 +0530

    base code ready

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,7 @@
 module.exports.home = function(req,res){
-    return res.end('<h1>Home controller Express is up for codeial</h1>')
+    return res.render('home.ejs',{
+        title: "Home"
+    })
 }
 
 module.exports.actionName = function(req,res){
