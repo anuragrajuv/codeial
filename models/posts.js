e66d5b5286032e29d6e14d975886b3e69f6a72b3
Anurag Raju <anuragrajuv@gmail.com>
2024-10-11 03:03:34 +0530

    Posts: Model Created, New Post form Added

---
+++ models/posts.js
@@ -0,0 +1,19 @@
+const { type } = require('express/lib/response');
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    }
+    
+},{
+    timestamps:true
+});
+
+const Post = mongoose.model('Post',postSchema);
+module.exports = Post;
