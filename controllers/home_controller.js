12acf9c42e9b686ca45adc07f29ca2c2b3e32b77
Anurag Raju <anuragrajuv@gmail.com>
2024-12-03 17:38:44 +0530

    AJAX intro

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,35 +2,6 @@ const Post = require("../models/post");
 const User = require("../models/user");
 const { posts } = require("./posts_controller");
 
-// module.exports.home = function(req,res){
-//     // Populate the user of each post
-//     Post.find({})
-//     .populate('user')
-//     .populate({
-//         path:'comments',
-//         populate:{
-//             path:'user'
-//         }
-//     })
-//     .exec()
-//     .then((posts)=>{
-//         User.find({})
-//         .then(users=>{
-//             return res.render('home',{
-//                 title:"Codeial|Home",
-//                 posts:posts,
-//                 all_users:users
-//             });
-//         })
-//         .catch(err=>{
-//             return console.error(err);            
-//         })
-//     })
-//     .catch((err)=>{
-//         return console.error(err);
-//     })
-// }
-
 // using async await to make the code smaller and easier to understand
 module.exports.home = async function(req,res){
 
