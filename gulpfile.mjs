4ceac8de4e6be68b92f3cc9b022b7792e10e709e
Anurag Raju <anuragrajuv@gmail.com>
2025-02-14 13:59:01 +0530

    getting production ready:GULP-public assets linking views_helper created

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,64 +1,72 @@
 import gulp from 'gulp';
 import sassPkg from 'gulp-sass';
 import cleanCSS from 'gulp-clean-css';
+import rev from 'gulp-rev';
 import terser from 'gulp-terser';
 import imagemin from 'gulp-imagemin';
 import { deleteAsync } from 'del';
 import * as dartSass from 'sass';
-import rev from 'gulp-rev';
 
 const sass = sassPkg(dartSass);
 
-// Paths
-const paths = {
-    styles: {
-        src: './assets/scss/**/*.scss',
-        dest: './public/assets/css/'
-    },
-    scripts: {
-        src: './assets/js/**/*.js',
-        dest: './public/assets/js/'
-    },
-    images: {
-        src: './assets/images/**/*.+(img|jpeg|jpg|png|gif|webp|tiff|eps|bmp)',
-        dest: './public/assets/images/'
-    }
-};
 
-function styles() {
-    return gulp.src(paths.styles.src)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(cleanCSS())
-        .pipe(rev())
-        .pipe(gulp.dest(paths.styles.dest));
-}
 
-// Minify JS, add hash
-function scripts() {
-    return gulp.src(paths.scripts.src)
-        .pipe(terser())
-        .pipe(rev())
-        .pipe(gulp.dest(paths.scripts.dest));
-}
 
-// Optimize Images
-function images() {
-    return gulp.src(paths.images.src)
-        .pipe(imagemin())
-        .pipe(gulp.dest(paths.images.dest));
-}
+gulp.task('css', function(done){
+    console.log('minifying css...');
+    gulp.src('./assets/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./assets/css'));
 
-// Clean Assets
-async function cleanAssets() {
-    await deleteAsync(['./public/assets']);
-}
+     gulp.src('./assets/**/*.css')
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done();
+});
 
-// Watch files
-function watchFiles() {
-    gulp.watch(paths.styles.src, styles);
-    gulp.watch(paths.scripts.src, scripts);
-    gulp.watch(paths.images.src, images);
-}
 
-// Default task
-export default gulp.series(cleanAssets, gulp.parallel(styles, scripts, images));
+gulp.task('js', function(done){
+    console.log('minifying js...');
+     gulp.src('./assets/**/*.js')
+    .pipe(terser())
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done()
+});
+
+
+gulp.task('images', function(done){
+    console.log('compressing images...');
+    gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
+    .pipe(imagemin())
+    .pipe(rev())
+    .pipe(gulp.dest('./public/assets'))
+    .pipe(rev.manifest({
+        cwd: 'public',
+        merge: true
+    }))
+    .pipe(gulp.dest('./public/assets'));
+    done();
+});
+
+// empty the public/assets directory
+gulp.task('clean:assets', async function(done){
+    await deleteAsync('./public/assets');
+    done();
+});
+
+gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images'), function(done){
+    console.log('Building assets');
+    done();
+});
